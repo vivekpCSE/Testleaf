eb02b3e31aa37d302f633f1a1b7747f995047644
vivekpCSE <132025482+vivekpCSE@users.noreply.github.com>
2025-10-10 09:08:00 +0530

    Add files via upload

---
+++ chrome.java
@@ -0,0 +1,17 @@
+public class Chrome {
+    public static void main(String[] args) {
+       
+        float version = 91.0f;
+        String developer = "Google";
+        boolean isBeta = false;
+        int releaseYear = 2008;
+        char shortcutKey = 'C';
+
+        
+        System.out.println("Version: " + version);
+        System.out.println("Developer: " + developer);
+        System.out.println("Is Beta: " + isBeta);
+        System.out.println("Release Year: " + releaseYear);
+        System.out.println("Shortcut Key: " + shortcutKey);
+    }
+}
